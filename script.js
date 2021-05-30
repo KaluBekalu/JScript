5147966ee0d66cabfd295ca742eeaeb172a7bd9b
kalubekalu <kalubekalu1@gmail.com>
2021-05-30 17:11:46 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const  cntdwn = () =>{
+const  date = new Date('Sep, 09, 2021 00:00:00').getTime()
+const now = new Date().getTime()
+const gap = date-gap;
+
+// Calculation
+
+
+console.log(date-now)
+
+}
+cntdwn()
