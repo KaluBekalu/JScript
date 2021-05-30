a9e6474e2bf3da787793a3a6afde932e6754166f
kalubekalu <kalubekalu1@gmail.com>
2021-05-30 20:25:12 +0300

    js function contruction

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
-const  cntdwn = () =>{
-const  date = new Date('Sep, 09, 2021 00:00:00').getTime()
-const now = new Date().getTime()
-const gap = date-gap;
+const cntdwn = () => {
+  const date = new Date("Sep, 09, 2021 00:00:00").getTime();
+  const now = new Date().getTime();
+  const gap = date - now;
 
-// Calculation
+  const ms = 1000;
+  const sec = ms * 60;
+  const min = sec * 60;
+  const hr = min * 60;
+  const dy = hr * 24;
 
-
-console.log(date-now)
-
-}
-cntdwn()
+  // Calculation
+  const day = Math.floor(gap / dy);
+  console.log(gap);
+};
+cntdwn();
